09797e574e068065fd87cd8c715af68b321b6ed5
azu <azuciao@gmail.com>
2022-10-28 10:33:50 +0900

    fix: support no-quoted echo

--- src/set-env-to-github_env.ts
+++ src/set-env-to-github_env.ts
@@ -1,14 +1,14 @@
 export function convert(content: string) {
     const ENV_PATTERN = [
-        /echo ["']::set-env name=(?<name>\${?.+}?|.+)::(?<variableName>\${?.+}?|.+)["']/g,
+        /echo\s+(["'])?::set-env name=(?<name>\${?.+}?|.+)::(?<variableName>\${?.+}?|.+)\1/g,
         `echo "$<name>=$<variableName>" >> $GITHUB_ENV`
     ] as const;
     const SAVE_PATTERN = [
-        /echo ["']::set-state name=(?<name>\${?.+}?|.+)::(?<variableName>\${?.+}?|.+)["']/g,
+        /echo\s+(["'])?::set-state name=(?<name>\${?.+}?|.+)::(?<variableName>\${?.+}?|.+)\1/g,
         `echo "$<name>=$<variableName>" >> $GITHUB_STATE`
     ] as const;
     const OUTPUT_PATTERN = [
-        /echo ["']::set-output name=(?<name>\${?.+}?|.+)::(?<variableName>\${?.+}?|.+)["']/g,
+        /echo\s+(["'])?::set-output name=(?<name>\${?.+}?|.+)::(?<variableName>\${?.+}?|.+)\1/g,
         `echo "$<name>=$<variableName>" >> $GITHUB_OUTPUT`
     ] as const;
     return [ENV_PATTERN, SAVE_PATTERN, OUTPUT_PATTERN].reduce((output, [pattern, replacement]) => {
